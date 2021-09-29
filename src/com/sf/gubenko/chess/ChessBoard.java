309235f7fbbad88bc1f3249efa0c96a6d4465c29
dmitriG <gubenko.dmitri@gmail.com>
2021-09-29 15:32:43 +0300

    Add to class ChessBoard fields: board, nowPlayer methods: nowPlayerColor, moveToPosition, printBoard

--- src/com/sf/gubenko/chess/ChessBoard.java
+++ src/com/sf/gubenko/chess/ChessBoard.java
@@ -1,4 +1,16 @@
 package com.sf.gubenko.chess;
 
 public class ChessBoard {
+    private ChessPiece[][] board;
+    private String nowPlayer;
+
+    public String nowPlayerColor() {
+        return nowPlayer;
+    }
+
+    public void moveToPosition() {
+    }
+
+    public void printBoard() {
+    }
 }
