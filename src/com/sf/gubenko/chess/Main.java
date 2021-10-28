491a8ed1221c93f495c15e7e36cf6a853cdc8592
gdm <nestora103@gmail.com>
2021-10-28 11:03:43 +0300

    add test coordinate

--- src/com/sf/gubenko/chess/Main.java
+++ src/com/sf/gubenko/chess/Main.java
@@ -4,54 +4,28 @@ public class Main {
     public static void main(String[] args) {
         ChessBoard board = new ChessBoard("w");
         Horse horse = new Horse("w");
-        System.out.println(horse.canMoveToPosition(board, 2,3, 1,1));
-        System.out.println(horse.canMoveToPosition(board, 2,3, 3,1));
-        System.out.println(horse.canMoveToPosition(board, 2,3, 4,4));
-        System.out.println(horse.canMoveToPosition(board, 2,3, 0,4));
-        System.out.println(horse.canMoveToPosition(board, 2,3, 3,5));
-        System.out.println(horse.canMoveToPosition(board, 2,3, 1,5));
-        System.out.println(horse.canMoveToPosition(board, 2,3, 1,5));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 1,1));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 3,1));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 4,4));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 0,4));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 3,5));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 1,5));
+//        System.out.println(horse.canMoveToPosition(board, 2,3, 1,5));
 
-        Bishop bishop = new Bishop("b");
+        Bishop bishop = new Bishop("w");
         System.out.println(bishop.getClass().getName());
-        System.out.println(bishop.canMoveToPosition(board, 3,4, 2,5));
-        System.out.println(bishop.canMoveToPosition(board, 3,4, 4,3));
-        System.out.println(bishop.canMoveToPosition(board, 3,4, 7,0));
-        System.out.println(bishop.canMoveToPosition(board, 3,4, 0,7));
-        System.out.println(bishop.canMoveToPosition(board, 3,4, 2,3));
-        System.out.println(bishop.canMoveToPosition(board, 3,4, 4,5));
-        System.out.println(bishop.getClass().getName());
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 2,4));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 4,2));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 5,1));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 1,5));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 0,6));
+
+        System.out.println(bishop.canMoveToPosition(board, 3,3, 3,3));
         System.out.println(bishop.canMoveToPosition(board, 3,3, 4,4));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 6,0));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 6,1));
-        //неверно!!
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 4,0));
-        System.out.println(bishop.canMoveToPosition(board, 3,3, 0,4));
-        System.out.println("====");
-        System.out.println(bishop.canMoveToPosition(board, 1,5, 0,4));
-        System.out.println(bishop.canMoveToPosition(board, 1,5, 0,6));
-        System.out.println(bishop.canMoveToPosition(board, 1,5, 3,7));
-        System.out.println(bishop.canMoveToPosition(board, 1,5, 3,3));
-        //неверно
-        System.out.println(bishop.canMoveToPosition(board, 1,5, 2,2));
-        System.out.println("====");
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 0,3));
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 4,7));
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 0,5));
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 5,0));
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 2,3));
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 2,5));
-        //неверно
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 7,0));
-        System.out.println(bishop.canMoveToPosition(board, 1,4, 0,7));
+        System.out.println(bishop.canMoveToPosition(board, 3,3, 7,7));
+        System.out.println(bishop.canMoveToPosition(board, 3,3, 2,2));
+        System.out.println(bishop.canMoveToPosition(board, 3,3, 0,0));
 
-
-
-
+        System.out.println(bishop.canMoveToPosition(board, 4,3, 3,4));
+        System.out.println(bishop.canMoveToPosition(board, 4,3, 0,7));
+        System.out.println(bishop.canMoveToPosition(board, 4,3, 5,2));
+        System.out.println(bishop.canMoveToPosition(board, 4,3, 7,0));
+        System.out.println(bishop.canMoveToPosition(board, 6,0, 7,1));
+        System.out.println(bishop.canMoveToPosition(board, 6,0, 1,7));
     }
 }
