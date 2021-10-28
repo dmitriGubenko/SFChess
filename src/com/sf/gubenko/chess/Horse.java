dc2a253fe403a3b6ad30027d25eeb4d7026c6745
gdm <nestora103@gmail.com>
2021-10-28 11:00:49 +0300

    add change horse business logic

--- src/com/sf/gubenko/chess/Horse.java
+++ src/com/sf/gubenko/chess/Horse.java
@@ -13,18 +13,9 @@ public class Horse extends ChessPiece {
 
     @Override
     boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if( !chessBoard.checkPos(line) ||
-            !chessBoard.checkPos(column) ||
-            !chessBoard.checkPos(toLine) ||
-            !chessBoard.checkPos(toColumn)) {
-            return false;
-        }
-        if((Math.abs(toLine - line) == 2 && Math.abs(toColumn - column) == 1) ||
-           (Math.abs(toLine - line) == 1 && Math.abs(toColumn - column) == 2)) {
-            return true;
-        }
-
-        return false;
+        return (chessBoard.isBasePos(line,column,toLine,toColumn)) &&
+                ((Math.abs(toLine - line) == 2 && Math.abs(toColumn - column) == 1) ||
+                        (Math.abs(toLine - line) == 1 && Math.abs(toColumn - column) == 2));
     }
 
     @Override
